6a9a77894fa055f16a777786324480db6e236a54
gk-svg <gk32239@gmail.com>
2020-06-18 18:27:53 +0530

    C/C++ pointers in depth

--- pointers.cpp
+++ pointers.cpp
@@ -8,9 +8,62 @@ int main(){
     p=arr[0];  //this is valid
     cout<<"Gives base address of arr: "<<p<<endl;
     cout<<"Gives base address value of arr: "<<*p<<endl;
-    cout<<"Gives base address of arr: "<<&p<<endl;
+    cout<<"Gives address of pointer: "<<&p<<endl;
     cout<<"Gives base address of arr: "<<arr<<endl;
     cout<<"Gives base address of arr: "<<*arr<<endl;
     cout<<"Gives base address of arr: "<<&arr<<endl;
+
+    //--------------------------1d dynamic array---------------------------
+    int n;
+    cin>>n;
+    int *num;
+    cout<<num<<endl;
+    num = new int[n];
+    cout<<num<<endl;
+    for (int i = 0; i < n; i++){
+        cin>>num[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<&num[i]<<'\t';
+    }
+    cout<<endl;
+
+    //--------------------------2d dynamic array--------------------------
+    int count;
+    cin>>count;
+    int **num1;
+    num1=new int*[n];
+    cout<<num1<<endl;
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+        num1[i]=new int[n];
+        cout<<num1[i]<<"\t";
+    }
+    cout<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        for (int j = 0; j < count; j++)
+        {
+            /* code */
+            cin>>num1[i][j];
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        for (int j = 0; j < count; j++)
+        {
+            /* code */
+            cout<<&num1[i][j]<<"\t";
+        }
+        
+    }
+    cout<<endl;
+    delete[] num;
+    delete[] num1;
     return 0;
 }
